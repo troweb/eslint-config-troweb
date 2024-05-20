8245237e3a28c91d4a4f822209a676cef16d331b
Hossein Saniei <h.saniei@gmail.com>
2024-05-20 12:35:06 +0330

    fix: turn dependencies to peer dependencies

---
+++ test/Component.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface Props {
+  name: string;
+}
+
+const Component: React.FC<Props> = ({ name }) => {
+  return <div>{name}</div>;
+};
+
+export default Component;
